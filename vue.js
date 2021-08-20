4efe93be7d5bfb7e7213702996510d1bb23e2bfd
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-08-20 01:01:12 -0400

    feature: mounted, created, methods

--- vue.js
+++ vue.js
@@ -1,40 +1,99 @@
 class Vue {
   constructor(config) {
     this.el = document.querySelector(config.el);
-		this.data = config.data;
+		this.$data = config.data;
+
+		if (config.mounted) {
+			this.$mount = config.mounted;
+		}
+
+		config.created?.();
 
 		const render = renderVue(this);
-
 		render();
-		defineReactive(this.data, render);
+
+		walkDataProps(this, render);
+		walkMethods(this, config.methods);
+		this.$mount?.();
   }
 }
 
-function renderVue(Vue) {
-	const originalInnerHTML = Vue.el.innerHTML;
+//#region rendering
+
+/**
+ * read the Vue.el and replace the HTML to the vue's data.
+ * @param {Vue} vue
+ * @returns {() => void}
+ */
+function renderVue(vue) {
+	const originalInnerHTML = vue.el.innerHTML;
 
 	return () => {
-		const { data } = Vue;
+		const { $data } = vue;
 
-		Vue.el.innerHTML = originalInnerHTML.replace(
+		vue.el.innerHTML = originalInnerHTML.replace(
 			/\{\{((?:.|\r?\n)+?)\}\}/g,
-			(_, val) => data[val.trim()]
+			(_, val) => $data[val.trim()]
 		);
 	}
 }
 
-function defineReactive(obj, cb) {
-	for (const key in obj) {
-		let value = obj[key];
-		Object.defineProperty(obj, key, {
-			get() {
-				return value;
-			},
-			set(newValue) {
-				if (value === newValue) return;
-				value = newValue;
-				cb()
-			}
-		})
+//#endregion
+
+//#region state
+
+/**
+ * read all props of a object and call defineReactive.
+ * @param {Vue} vue
+ * @param {Function} cb
+ */
+function walkDataProps(vue, cb) {
+	for (const key in vue.$data) {
+		defineReactive(vue, key, cb);
+		defineReactive(vue, key, cb, true);
 	}
 }
+
+/**
+ * change set behavior and define props for Vue and Vue.data from Vue.data
+ * @param {Vue} obj
+ * @param {string} key
+ * @param {Function} cb
+ * @param {boolean} redefineData
+ */
+function defineReactive(obj, key, cb, redefineData) {
+	let value = obj.$data[key];
+
+	Object.defineProperty(redefineData ? obj.$data : obj, key, {
+		get() {
+			return value;
+		},
+		set(newValue) {
+			if (value === newValue) return;
+			value = newValue;
+
+			if (!redefineData) {
+				obj.$data[key] = value;
+			}
+			else {
+				obj[key] = value;
+			}
+
+			if (!redefineData) {
+				cb();
+			}
+		}
+	})
+}
+
+//#endregion
+
+//#region methods
+
+function walkMethods(vue, methods) {
+	for (const key in methods) {
+		vue[key] = methods[key];
+	}
+}
+
+//#endregion
